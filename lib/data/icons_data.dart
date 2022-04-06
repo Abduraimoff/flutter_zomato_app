391c8eabfcc8fc648e4a450a5b1fd4c117f3548a
Abduraimoff <abdurayimovmansur3@gmail.com>
2022-04-06 16:45:25 +0500

    icons data

---
+++ lib/data/icons_data.dart
@@ -0,0 +1,75 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:zomato_app/app_color.dart';
+
+List icons = [
+  {
+    'active': SvgPicture.asset(
+      'assets/icons/bag2.svg',
+      color: AppColor.kRedColor,
+      width: 30,
+      height: 30,
+    ),
+    'inactive': SvgPicture.asset(
+      'assets/icons/bag2.svg',
+      color: AppColor.kyGreyColor,
+      width: 30,
+      height: 30,
+    ),
+  },
+  {
+    'active': SvgPicture.asset(
+      'assets/icons/bubble.svg',
+      color: AppColor.kRedColor,
+      width: 30,
+      height: 30,
+    ),
+    'inactive': SvgPicture.asset(
+      'assets/icons/bubble.svg',
+      color: AppColor.kyGreyColor,
+      width: 30,
+      height: 30,
+    ),
+  },
+  {
+    'active': SvgPicture.asset(
+      'assets/icons/crown.svg',
+      color: AppColor.kRedColor,
+      width: 30,
+      height: 30,
+    ),
+    'inactive': SvgPicture.asset(
+      'assets/icons/crown.svg',
+      color: AppColor.kyGreyColor,
+      width: 30,
+      height: 30,
+    ),
+  },
+  {
+    'active': SvgPicture.asset(
+      'assets/icons/shop.svg',
+      color: AppColor.kRedColor,
+      width: 30,
+      height: 30,
+    ),
+    'inactive': SvgPicture.asset(
+      'assets/icons/shop.svg',
+      color: AppColor.kyGreyColor,
+      width: 30,
+      height: 30,
+    ),
+  },
+  {
+    'active': SvgPicture.asset(
+      'assets/icons/heart.svg',
+      color: AppColor.kRedColor,
+      width: 30,
+      height: 30,
+    ),
+    'inactive': SvgPicture.asset(
+      'assets/icons/heart.svg',
+      color: AppColor.kyGreyColor,
+      width: 30,
+      height: 30,
+    ),
+  },
+];
